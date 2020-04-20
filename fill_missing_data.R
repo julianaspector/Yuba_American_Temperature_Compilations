101e98a38d89c9e85a449adcfe45af338cb4b2ad
Juliana Spector <julianaspector@gmail.com>
2020-04-20 15:40:03 -0700

    created directory

--- fill_missing_data.R
+++
@@ -1,6 +0,0 @@
-
-fill_missing_data <- function(df, row, rowL, max, min, mean){
-  df[row,] <- df %>% slice(rowL) %>%  
-  replace_na(list(Max=max, Min=min, Mean=mean))
-  return(df)
-}
--- find_narows.R
+++
@@ -1,4 +0,0 @@
-find_narows <- function(new_df, df){
-  new_df <- df[rowSums(is.na(df)) > 0,]
-  return(new_df)
-}
